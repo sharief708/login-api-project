eb8b92bbd4fcd630101cd0e1eb42e9987d97a9d3
Umar Sharief <umarsharief708@gmail.com>
2026-03-09 13:06:12 +0530

    Add login API

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (username === "admin" && password === "1234") {
+    return res.json({ message: "Login successful" });
+  }
+
+  res.status(401).json({ message: "Invalid credentials" });
+});
+
+app.listen(3000, () => {
+  console.log("Login API feature branch update");
+  console.log("Login API update for PR");
+});
